9d52f65591b720a7ac9ca2d2d17c73f3df89d8c5
FELIX FREIDMAN <felixfreidman@gmail.com>
2023-05-05 14:31:09 +0300

    2:30PM Commit Добавил изменение лчиных данных в кабинете

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -20,9 +20,39 @@ if (pageLocation.includes('profile')) {
   });
 }
 
-if (localStorage.getItem('logged')) {
-  console.log(localStorage.getItem('logged'));
+var editIcons = document.querySelectorAll('.change-icon');
+editIcons.forEach(function (icon) {
+  icon.addEventListener('click', function () {
+    var ID = icon.id;
 
+    if (ID !== 'changePassword') {
+      var editValueContainer = document.querySelector(".".concat(ID));
+      var valueTextContainer = editValueContainer.querySelector('span');
+      var value = valueTextContainer.textContent.trim();
+      var editInput = document.createElement('input');
+      valueTextContainer.classList.add('js--hidden');
+      icon.classList.add('js--hidden');
+      editInput.value = value;
+      editInput.type = 'text';
+      editInput.classList.add('editInput');
+      editValueContainer.appendChild(editInput);
+    } else {
+      var _editValueContainer = document.querySelector(".".concat(ID));
+
+      var valueContainer = _editValueContainer.querySelector('.info-value');
+
+      var labelContainer = _editValueContainer.querySelector('.info-label');
+
+      var passwordContainer = _editValueContainer.querySelector('.info-password');
+
+      valueContainer.classList.add('js--hidden');
+      labelContainer.classList.add('js--hidden');
+      passwordContainer.classList.remove('js--hidden');
+    }
+  });
+});
+
+if (localStorage.getItem('logged')) {
   if (localStorage.getItem('logged') == 'true') {
     var account = document.querySelector('.account');
     account.classList.add('account--logged');
--- src/assets/js/components/logStatus.js
+++ src/assets/js/components/logStatus.js
@@ -1,5 +1,4 @@
 if (localStorage.getItem('logged')) {
-    console.log(localStorage.getItem('logged'));
     if (localStorage.getItem('logged') == 'true') {
         const account = document.querySelector('.account');
         account.classList.add('account--logged');
