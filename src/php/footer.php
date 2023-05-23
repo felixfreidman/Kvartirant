95822cfc8a3fb8ab99a77c05ab61eb3d8c3e9782
FELIX FREIDMAN <felixfreidman@gmail.com>
2023-05-23 18:37:10 +0300

    6:36PM Commit Design-Prototype

--- src/php/footer.php
+++ src/php/footer.php
@@ -139,10 +139,8 @@
     </a>
     <?php }  ?>
 </div>
-<!-- <script src="<?php echo get_template_directory_uri() . '/assets/js/jquery.min.js' ?>"> </script> -->
 <script src="<?php echo get_template_directory_uri() . '/assets/js/plugins.min.js' ?>"> </script>
 <script src="<?php echo get_template_directory_uri() . '/assets/js/main.min.js' ?>"> </script>
-
 </body>
 
 </html>
